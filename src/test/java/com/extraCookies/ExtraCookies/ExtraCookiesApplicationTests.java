8ccc738a0bd01915f921d6300a14003f23c15897
Devi <diptikadevi@gmail.com>
2024-08-17 13:24:37 -0400

    initial commit

---
+++ src/test/java/com/extraCookies/ExtraCookies/ExtraCookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extraCookies.ExtraCookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraCookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
